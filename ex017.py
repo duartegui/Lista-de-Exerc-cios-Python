710439814c77258e6743b481afba54c2d7e04a0b
duartegui <guilhermejrduarte@gmail.com>
2023-09-19 13:21:16 -0300

    adicionando resolução faltante

--- ex017.py
+++ ex017.py
@@ -1,3 +1,11 @@
 # Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo, calcule
 # e mostre o comprimento da hipotenusa.
+import math
+cateto_oposto = float(input('Digite o comprimento do cateto oposto: '))
+cateto_adjacente = (float(input('Digite o valor do cateto adjacente: ')))
+'''hipotenusa = (cateto_oposto ** 2 + cateto_adjacente ** 2) ** (1/2) #elevar a meio é uma forma de calcular a raiz quadrada de um número
 
+print(f'O valor da hipotenusa é: {hipotenusa:.2f}')''' #essa é a forma de resolver realizando os cálculos matemáticos
+
+hipotenusa = math.hypot (cateto_oposto, cateto_adjacente)
+print(f'O valor da hipotenusa é de: {hipotenusa:.2f}')
--- ex072Tuplas.py
+++ ex072Tuplas.py
@@ -1,4 +1,6 @@
-'''Exercício Python 72: Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por extenso, de zero até vinte. Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.'''
+'''Exercício Python 72: Crie um programa que tenha uma dupla totalmente
+ preenchida com uma contagem por extenso, de zero até vinte. Seu programa 
+ deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.'''
 
 contador = ('zero', 'um', 'dois', 'três', 'quatro',
             'cinco', 'seis', 'oito', 'nove', 'dez',
